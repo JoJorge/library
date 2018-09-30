89d91a5b63bf040e3f3337b7998acb0f20f4749a
Jorge Lin <jorge13jorge13@gmail.com>
2018-09-30 11:22:23 +0800

    add windows PowerShell script

---
+++ Microsoft.PowerShell_profile.ps1
@@ -0,0 +1,15 @@
+﻿
+# Load posh-git example profile, which will setup a prompt
+#. 'C:\Users\JoJorge\Documents\WindowsPowerShell\Modules\posh-git\profile.example.ps1'
+$poshGitModule = Get-Module posh-git -ListAvailable | Sort-Object Version -Descending | Select-Object -First 1
+if ($poshGitModule) {
+    $poshGitModule | Import-Module
+}
+elseif (Test-Path -LiteralPath ($modulePath = Join-Path (Split-Path $MyInvocation.MyCommand.Path -Parent) (Join-Path src 'posh-git.psd1'))) {
+    Import-Module $modulePath
+}
+else {
+    throw "Failed to import posh-git."
+}
+
+#& 'C:\Program Files\Git\usr\bin\ssh-add.exe'
